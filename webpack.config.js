653dbe75659bf548e7d84d07c4f513c6c73d8140
elaine1129 <elainelowjingyi00@gmail.com>
2022-11-27 15:03:10 +0800

    cache busting and HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 module.exports = {
   mode: "development", // so that the main.js is no longer minify
   // devtool: false, //get rid of eval(), make it to something we understand
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "main.[contenthash].js",
     path: path.resolve(__dirname, "dist"), // create a dist directory and output to main.js inside the dir
     //__dirname means to take user's directory and create dist folder, cause cannot hardcode our directory
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
   module: {
     rules: [
       {
